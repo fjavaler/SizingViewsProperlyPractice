262cd71438c41921af3212e612161dba0aa2910a
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-26 20:57:15 -0600

    Initial Commit

---
+++ Shared/SizingViewsProperlyPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SizingViewsProperlyPracticeApp.swift
+//  Shared
+//
+//  Created by Fred Javalera on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SizingViewsProperlyPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
